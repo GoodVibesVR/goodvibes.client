a0aad347497bea1644bf9fc1c751ab9d339044d4
PhaxeNor <PhaxeNor@users.noreply.github.com>
2022-06-16 22:30:03 +0200

    Avatar Mapper Combobox

--- GoodVibes.Client.Wpf/Modules/AvatarMapperModule/ViewModels/AvatarMapperViewModel.cs
+++ GoodVibes.Client.Wpf/Modules/AvatarMapperModule/ViewModels/AvatarMapperViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 using GoodVibes.Client.Core.Mvvm;
 using GoodVibes.Client.Lovense.Enums;
 using GoodVibes.Client.Lovense.EventCarriers;
@@ -30,64 +31,72 @@ namespace GoodVibes.Client.Wpf.Modules.AvatarMapperModule.ViewModels
 
         private void AvatarChanged(AvatarChangedEvent obj)
         {
-            Console.WriteLine("AvatarChanged event received");
-            var exists = Avatars.Any(a => a.AvatarId == obj.AvatarId);
-            if (!exists)
+            Application.Current.Dispatcher.Invoke((Action)delegate
             {
-                Console.WriteLine($"Adding new avatar with ID {obj.AvatarId} to Avatar list");
-                Avatars.Add(new AvatarMappingDto()
+                Console.WriteLine("AvatarChanged event received");
+
+                var exists = Avatars.Any(a => a.AvatarId == obj.AvatarId);
+                if (!exists)
                 {
-                    AvatarId = obj.AvatarId
-                });
-            }
+                    Console.WriteLine($"Adding new avatar with ID {obj.AvatarId} to Avatar list");
+                    Avatars.Add(new AvatarMappingDto()
+                    {
+                        AvatarId = obj.AvatarId
+                    });
+                }
+            });
         }
 
         private void LovenseToyListUpdated(LovenseToyListUpdatedEvent obj)
         {
-            var tempList = Toys;
-            var tempList2 = new List<ToyMappingDto>();
-            foreach (var lovenseToy in obj.ToyList!)
+            Application.Current.Dispatcher.Invoke((Action)delegate
             {
-                if (!lovenseToy.Status || !lovenseToy.Enabled)
+                var tempList = Toys;
+                var tempList2 = new List<ToyMappingDto>();
+                foreach (var lovenseToy in obj.ToyList!)
                 {
-                    continue;
-                }
-
-                if (lovenseToy.Function1 != LovenseCommandEnum.None)
-                {
-                    tempList2.Add(new ToyMappingDto()
+                    if (!lovenseToy.Status || !lovenseToy.Enabled)
                     {
-                        DisplayName = lovenseToy.DisplayName,
-                        Function = lovenseToy.Function1,
-                        Id = lovenseToy.Id!
-                    });
-                }
+                        continue;
+                    }
 
-                if (lovenseToy.Function2 != LovenseCommandEnum.None)
-                {
-                    tempList2.Add(new ToyMappingDto()
+                    if (lovenseToy.Function1 != LovenseCommandEnum.None)
                     {
-                        DisplayName = lovenseToy.DisplayName,
-                        Function = lovenseToy.Function2,
-                        Id = lovenseToy.Id!
-                    });
-                }
-            }
+                        tempList2.Add(new ToyMappingDto()
+                        {
+                            DisplayName = lovenseToy.DisplayName,
+                            Function = lovenseToy.Function1,
+                            Id = lovenseToy.Id!
+                        });
+                    }
 
-            foreach (var toy in Toys)
-            {
-                var exists = tempList2.Any(t => t.Id == toy.Id && t.Function == toy.Function);
-                if (!exists)
+                    if (lovenseToy.Function2 != LovenseCommandEnum.None)
+                    {
+                        tempList2.Add(new ToyMappingDto()
+                        {
+                            DisplayName = lovenseToy.DisplayName,
+                            Function = lovenseToy.Function2,
+                            Id = lovenseToy.Id!
+                        });
+                    }
+                }
+
+                foreach (var toy in Toys)
                 {
-                    Toys.Add(toy);
+                    var exists = tempList2.Any(t => t.Id == toy.Id && t.Function == toy.Function);
+                    if (!exists)
+                    {
+                        Toys.Add(toy);
+                    }
+
+                    // TODO: Actually remove as well.
                 }
 
-                // TODO: Actually remove as well.
-            }
-
-            // TODO: We need to handle mapped toy types as well as IDs here... or?
-            //var disconnectedToys = tempList.Where(t => obj.ToyList.All(x => x.Id != t.Id));
-            //var test = tempList.Except(Toys);
+                // TODO: We need to handle mapped toy types as well as IDs here... or?
+                //var disconnectedToys = tempList.Where(t => obj.ToyList.All(x => x.Id != t.Id));
+                //var test = tempList.Except(Toys);
+            });
+            
         }
 
         public override void OnNavigatedTo(NavigationContext navigationContext)
