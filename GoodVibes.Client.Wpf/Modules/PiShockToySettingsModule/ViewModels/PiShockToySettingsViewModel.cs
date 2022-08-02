fa5c392ff5d9866c25926553a2e4db2cc1e18454
Miwca <michaela@skeppstedt.org>
2022-08-02 21:19:20 +0200

    Added properties to PiShock viewmodel

--- GoodVibes.Client.Wpf/Modules/PiShockToySettingsModule/ViewModels/PiShockToySettingsViewModel.cs
+++ GoodVibes.Client.Wpf/Modules/PiShockToySettingsModule/ViewModels/PiShockToySettingsViewModel.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GoodVibes.Client.Core.Mvvm;
+﻿using GoodVibes.Client.Core.Mvvm;
 using GoodVibes.Client.PiShock;
+using Prism.Commands;
 using Prism.Regions;
 
 namespace GoodVibes.Client.Wpf.Modules.PiShockToySettingsModule.ViewModels
@@ -12,6 +8,34 @@ namespace GoodVibes.Client.Wpf.Modules.PiShockToySettingsModule.ViewModels
     public class PiShockToySettingsViewModel : RegionViewModelBase
     {
         private readonly PiShockClient _piShockClient;
+
+        private int _duration;
+
+        public int Duration
+        {
+            get => _duration;
+            set => SetProperty(ref _duration, value);
+        }
+
+        private int _intensity;
+
+        public int Intensity
+        {
+            get => _intensity;
+            set => SetProperty(ref _intensity, value);
+        }
+
+        private DelegateCommand _shockCommand;
+        public DelegateCommand ShockCommand =>
+            _shockCommand ??= new DelegateCommand(Shock);
+
+        private DelegateCommand _vibrateCommand;
+        public DelegateCommand VibrateCommand =>
+            _vibrateCommand ??= new DelegateCommand(Vibrate);
+
+        private DelegateCommand _beepCommand;
+        public DelegateCommand beepCommand =>
+            _beepCommand ??= new DelegateCommand(Beep);
 
         public PiShockToySettingsViewModel(IRegionManager regionManager, PiShockClient piShockClient) : base(regionManager)
         {
@@ -28,6 +52,21 @@ namespace GoodVibes.Client.Wpf.Modules.PiShockToySettingsModule.ViewModels
             }
 
             // TODO: ....
+        }
+
+        private void Shock()
+        {
+
+        }
+
+        private void Vibrate()
+        {
+
+        }
+
+        private void Beep()
+        {
+
         }
     }
 }
