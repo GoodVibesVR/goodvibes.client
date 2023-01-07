f7202bd4d81f51da371e92629c3edfc7496d3d31
Michaela <michaela@skeppstedt.org>
2023-01-07 19:22:56 +0100

    Changed Dolce and Edge to singular function toys (#7)

--- GoodVibes.Client.Lovense/Models/Edge.cs
+++ GoodVibes.Client.Lovense/Models/Edge.cs
@@ -20,7 +20,7 @@ public class Edge : LovenseToy
     public Edge() : base()
     {
         Enabled = true;
-        Function1 = LovenseCommandEnum.Vibrate1;
-        Function2 = LovenseCommandEnum.Vibrate2;
+        Function1 = LovenseCommandEnum.Vibrate;
+        Function2 = LovenseCommandEnum.None;
     }
 }
