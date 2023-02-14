0cbc894ade4b7fdd81277f48c2b2265f2124b5fe
Miwca <michaela@skeppstedt.org>
2023-02-14 23:22:34 +0100

    Quick refactoring

--- GoodVibes.Client.Serial/SerialClient.cs
+++ GoodVibes.Client.Serial/SerialClient.cs
@@ -11,16 +11,14 @@ public class SerialClient
 {
     private readonly SerialSettings _serialSettings;
 
-    private SerialPort serialPort = null!;
-    private Thread _thread = null!;
-    //private string receivedData = null!;
+    private SerialPort _serialPort = null!;
 
     public SerialClient(ApplicationSettings applicationSettings)
     {
         _serialSettings = applicationSettings.SerialSettings!;
 
         var comPorts = new List<string>();
-        foreach (var serialSettingsDeviceId in _serialSettings.DeviceIds)
+        foreach (var serialSettingsDeviceId in _serialSettings.DeviceIds!)
         {
             var vidAndPid = serialSettingsDeviceId.Split(":");
             comPorts.AddRange(GetPiShockComPorts(Convert.ToInt32(vidAndPid[0]), Convert.ToInt32(vidAndPid[1])));
@@ -32,7 +30,7 @@ public class SerialClient
         }
     }
 
-    static IEnumerable<string> GetPiShockComPorts(int vid, int pid)
+    private static IEnumerable<string> GetPiShockComPorts(int vid, int pid)
     {
         var pattern = $"^VID_{vid:X}.PID_{pid:X}";
         var rx = new Regex(pattern, RegexOptions.IgnoreCase);
@@ -40,24 +38,33 @@ public class SerialClient
 
         var rk1 = Registry.LocalMachine;
         var rk2 = rk1.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum");
+        if (rk2 == null) return comports;
 
         foreach (var s3 in rk2.GetSubKeyNames())
         {
             var rk3 = rk2.OpenSubKey(s3);
+            if (rk3 == null) continue;
+
             foreach (var s in rk3.GetSubKeyNames())
             {
-                if (rx.Match(s).Success)
+                if (!rx.Match(s).Success) continue;
+
+                var rk4 = rk3.OpenSubKey(s);
+                if (rk4 == null) continue;
+
+                foreach (var s2 in rk4.GetSubKeyNames())
                 {
-                    var rk4 = rk3.OpenSubKey(s);
-                    foreach (var s2 in rk4.GetSubKeyNames())
-                    {
-                        var rk5 = rk4.OpenSubKey(s2);
-                        var location = (string)rk5.GetValue("LocationInformation");
-                        var rk6 = rk5.OpenSubKey("Device Parameters");
-                        var portName = (string)rk6.GetValue("PortName");
-                        if (!string.IsNullOrEmpty(portName) && SerialPort.GetPortNames().Contains(portName))
-                            comports.Add((string)rk6.GetValue("PortName"));
-                    }
+                    var rk5 = rk4.OpenSubKey(s2);
+                    var rk6 = rk5?.OpenSubKey("Device Parameters");
+                    if (rk6 == null) continue;
+
+                    var portName = (string)rk6.GetValue("PortName")!;
+                    if (string.IsNullOrEmpty(portName) || !SerialPort.GetPortNames().Contains(portName)) continue;
+
+                    var comPort = (string)rk6.GetValue("PortName")!;
+                    if (string.IsNullOrEmpty(comPort)) continue;
+
+                    comports.Add(comPort);
                 }
             }
         }
@@ -67,27 +74,27 @@ public class SerialClient
 
     private void ConnectToPort(string comPort)
     {
-        serialPort = new SerialPort();
-        serialPort.BaudRate = _serialSettings.BaudRate;
-        serialPort.PortName = comPort;
-        serialPort.Parity = Parity.None;
-        serialPort.DataBits = 8;
-        serialPort.StopBits = StopBits.One;
-        serialPort.ReadBufferSize = 20000000;
-        serialPort.DataReceived += SerialPort_DataReceived;
+        _serialPort = new SerialPort();
+        _serialPort.BaudRate = _serialSettings.BaudRate;
+        _serialPort.PortName = comPort;
+        _serialPort.Parity = Parity.None;
+        _serialPort.DataBits = 8;
+        _serialPort.StopBits = StopBits.One;
+        _serialPort.ReadBufferSize = 20000000;
+        _serialPort.DataReceived += SerialPort_DataReceived;
 
-        serialPort.Open();
+        _serialPort.Open();
     }
 
     private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
     {
-        var data = serialPort.ReadLine();
+        var data = _serialPort.ReadLine();
         var correctJson = data.TryParseJson<PiShockSerialCallback>(out var correctData);
         if (correctJson)
         {
             if (correctData.Commands.Any())
             {
-                
+                // TODO: Do stuff
             }
         }
     }
