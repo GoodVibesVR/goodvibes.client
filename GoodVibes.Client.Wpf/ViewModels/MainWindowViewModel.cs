06f462dfd9401590089c1c240f367b5f389bf986
Michaela <michaela@skeppstedt.org>
2023-07-13 22:35:49 +0200

    Updated version to 1.5 (#12)
    Updated version

--- GoodVibes.Client.Wpf/ViewModels/MainWindowViewModel.cs
+++ GoodVibes.Client.Wpf/ViewModels/MainWindowViewModel.cs
@@ -10,7 +10,7 @@ namespace GoodVibes.Client.Wpf.ViewModels
         private readonly LovenseEventHandler _lovenseEventHandler;
         private readonly PiShockEventHandler _piShockEventHandler;
 
-        private string _title = "GoodVibes 1.0-alpha1.4";
+        private string _title = "GoodVibes 1.0-alpha1.5";
         public string Title
         {
             get => _title;
