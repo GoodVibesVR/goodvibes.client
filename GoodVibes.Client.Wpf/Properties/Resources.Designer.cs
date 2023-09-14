9707c51f6427a010164bc69ade341b11299bc460
Miwca <michaela@skeppstedt.org>
2023-09-14 18:49:39 +0200

    Added Ridge image to settings page

--- GoodVibes.Client.Wpf/Properties/Resources.Designer.cs
+++ GoodVibes.Client.Wpf/Properties/Resources.Designer.cs
@@ -593,6 +593,16 @@ namespace GoodVibes.Client.Wpf.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap image_ridge {
+            get {
+                object obj = ResourceManager.GetObject("image_ridge", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap image_sexmachine {
             get {
                 object obj = ResourceManager.GetObject("image_sexmachine", resourceCulture);
